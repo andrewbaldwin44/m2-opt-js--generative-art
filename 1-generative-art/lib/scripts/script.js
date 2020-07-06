045e54ea5a951440fcd7e788db340dd76f261563
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-05 20:56:30 -0400

    Exercise 3: Add new art variables

--- 1-generative-art/lib/scripts/script.js
+++ 1-generative-art/lib/scripts/script.js
@@ -6,6 +6,8 @@ function generateArt() {
         const posY = `${Math.random() * 100}%`;
         const randomColor = colors[Math.floor(Math.random() * colors.length)];
         const randomRotation = `${Math.random() * 360}deg`;
+        const randomBlend = blendModes[Math.floor(Math.random() * blendModes.length)];
+        const randomSkew = `${Math.random() * 360}rad`;
 
         const newDiv = document.createElement('div');
         newDiv.style.position = 'absolute';
@@ -14,7 +16,8 @@ function generateArt() {
         newDiv.style.left = posX;
         newDiv.style.top = posY;
         newDiv.style.backgroundColor = randomColor;
-        newDiv.style.transform = `rotate(${randomRotation})`;
+        newDiv.style.transform = `rotate(${randomRotation}) skew(${randomSkew})`;
+        newDiv.style.mixBlendMode = randomBlend;
         newDiv.style.zIndex = 500;
         
         canvas.appendChild(newDiv);
@@ -29,6 +32,10 @@ function regenerateArt() {
 const canvas = document.querySelector('#canvas');
 const regenerateButton = document.querySelector('#regenerate');
 const colors = ['#FFBE0B', '#FB5607', '#FF006E', '#8338EC', '#3A86FF'];
+const blendModes = ['normal', 'multiply', 'screen', 'overlay', 'darken', 
+                    'lighten', 'color-dodge', 'color-burn', 'hard-light',
+                    'soft-light', 'difference', 'exclusion', 'hue',
+                    'saturation', 'color', 'luminosity'];
 
 generateArt();
 
